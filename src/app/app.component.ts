44d0d231ba103de100dcce999a66468385043f8a
Krystian Sudoł <45632898+krycha99@users.noreply.github.com>
2022-11-07 07:01:48 +0100

    base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const L: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'WhereIsRain';
+
+  ngOnInit(){
+    
+    //Checking user location
+    if(!navigator.geolocation){
+      console.log('location is not supported');
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      // console.log(`lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`);
+
+     //map open
+    var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 15);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    //geocoding plugin(searchbar on map)
+   L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+     attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+   }).addTo(map);
+   L.Control.geocoder().addTo(map);
+
+   });
+
+   
+    
+   
+
+  }
 }
