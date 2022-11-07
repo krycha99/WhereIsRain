100106cd61e8a200451178a671100a489b16f844
Krystian Sudoł <45632898+krycha99@users.noreply.github.com>
2022-11-07 07:56:57 +0100

    weather widget with static data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit{
       console.log('location is not supported');
     }
     navigator.geolocation.getCurrentPosition((position) => {
-      // console.log(`lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`);test
+      // console.log(`lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`);
 
      //map open
     var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 15);
@@ -31,6 +31,23 @@ export class AppComponent implements OnInit{
      attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
    }).addTo(map);
    L.Control.geocoder().addTo(map);
+
+   //adding custom marker at startpoint
+   const url =  'assets/rainy-1.svg';
+    var customIcon = L.icon({
+      
+      iconUrl: url,
+      iconSize:     [100, 150], // size of the icon
+      shadowSize:   [50, 64], // size of the shadow
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      shadowAnchor: [4, 62],  // the same for the shadow
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
+  L.marker([position.coords.latitude, position.coords.longitude], {icon: customIcon}, {alt: 'ogór rick'}).addTo(map) .bindPopup('You are here :)') ;
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
 
    });
 
