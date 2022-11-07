95ce196ac7168be33894ccd5551a98215942bb1b
Krystian Sudoł <45632898+krycha99@users.noreply.github.com>
2022-11-07 13:03:30 +0100

    apis connected

--- src/app/Components/weather-widget/weather-widget.component.ts
+++ src/app/Components/weather-widget/weather-widget.component.ts
@@ -6,19 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-widget.component.css']
 })
 export class WeatherWidgetComponent implements OnInit {
-
+  
   WeatherData:any;
   constructor() { }
 
   ngOnInit(): void {
+    //default values
+    let data = JSON.parse('{"coord":{"lon":72.85,"lat":19.01},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],"base":"stations","main":{"temp":297.15,"feels_like":297.4,"temp_min":297.15,"temp_max":297.15,"pressure":1013,"humidity":69},"visibility":3500,"wind":{"speed":3.6,"deg":300},"clouds":{"all":20},"dt":1580141589,"sys":{"type":1,"id":9052,"country":"IN","sunrise":1580089441,"sunset":1580129884},"timezone":19800,"id":1275339,"name":"Mumbai","cod":200}');
+    this.setWeatherData(data);
     this.getWeatherData();
     console.log(this.WeatherData);
     console.log("tutej" + this.WeatherData.weather);
+    // const span = document.getElementById('cityName') as HTMLDivElement | null;
+  setInterval(() => {
+    const span = document.getElementById('cityName') as HTMLDivElement | null;
+    if(span?.innerHTML != this.WeatherData.name){
+      console.log("tu zmieniam elo " + span?.innerHTML + " " +  this.WeatherData.name);
+      this.getWeatherDataByName(span?.innerHTML);
+    }
+    else{
+      console.log("wszystko sie zgadza nic nie zmieniam");
+    }
+    
+    // const siemanko = span?.innerHTML;
+    // console.log(siemanko);
+    // this.getWeatherDataByName(siemanko);
+    }, 3000);
   }
 
   getWeatherData(){
-    let data = JSON.parse('{"coord":{"lon":72.85,"lat":19.01},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],"base":"stations","main":{"temp":297.15,"feels_like":297.4,"temp_min":297.15,"temp_max":297.15,"pressure":1013,"humidity":69},"visibility":3500,"wind":{"speed":3.6,"deg":300},"clouds":{"all":20},"dt":1580141589,"sys":{"type":1,"id":9052,"country":"IN","sunrise":1580089441,"sunset":1580129884},"timezone":19800,"id":1275339,"name":"Mumbai","cod":200}');
-    this.setWeatherData(data);
+    //YXBwaWQ9Y2ZhZTJlMjFkN2JlZDc0NWVlOGU3ZjJiZTYxMWU5ZTI=
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=London&' + window.atob("YXBwaWQ9Y2ZhZTJlMjFkN2JlZDc0NWVlOGU3ZjJiZTYxMWU5ZTI="))
+    .then(response=>response.json())
+    .then(data=>{this.setWeatherData(data);})
+    // let data = JSON.parse('{"coord":{"lon":72.85,"lat":19.01},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],"base":"stations","main":{"temp":297.15,"feels_like":297.4,"temp_min":297.15,"temp_max":297.15,"pressure":1013,"humidity":69},"visibility":3500,"wind":{"speed":3.6,"deg":300},"clouds":{"all":20},"dt":1580141589,"sys":{"type":1,"id":9052,"country":"IN","sunrise":1580089441,"sunset":1580129884},"timezone":19800,"id":1275339,"name":"Mumbai","cod":200}');
+    // this.setWeatherData(data);
+
+  }
+
+  getWeatherDataByName(city: any){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city +'&' + window.atob("YXBwaWQ9Y2ZhZTJlMjFkN2JlZDc0NWVlOGU3ZjJiZTYxMWU5ZTI="))
+    .then(response=>response.json())
+    .then(data=>{this.setWeatherData(data);})
 
   }
 
@@ -32,7 +61,17 @@ export class WeatherWidgetComponent implements OnInit {
     this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
     this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
     this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
-    this.WeatherData.weatherType = this.WeatherData.weather.main;
+    this.WeatherData.weatherType = this.WeatherData.weather[0].main;
+    console.log("tutaj " + this.WeatherData.weatherType);
+    if(this.WeatherData.weatherType != "Clear")
+    {
+      this.WeatherData.isCloud = true;
+    }
+    else{
+      this.WeatherData.isCloud = false;
+    }
+    
+    
    
   }
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherWidgetComponent } from "./Components/weather-widget/weather-widget.component"
 
 declare const L: any;
 
@@ -11,14 +12,19 @@ export class AppComponent implements OnInit{
   title = 'WhereIsRain';
 
   ngOnInit(){
-    
     //Checking user location
     if(!navigator.geolocation){
       console.log('location is not supported');
     }
     navigator.geolocation.getCurrentPosition((position) => {
       // console.log(`lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`);
-
+        var crd = position.coords;
+        var lat = crd.latitude.toString();
+        var lng = crd.longitude.toString();
+        var coordinates = [lat, lng];
+      this.getCity(coordinates);
+      //ustawiamy miasto
+      
      //map open
     var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 15);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -30,24 +36,103 @@ export class AppComponent implements OnInit{
    L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
      attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
    }).addTo(map);
-   L.Control.geocoder().addTo(map);
+
+   //custom marker on searching
+   var geocoder = L.Control.geocoder({
+    defaultMarkGeocode: false
+  })
+    .on('markgeocode', function(e: { geocode: { name: any; center: any; bbox: any;  }; }) {
+
+
+      //sprawdzanie pogody po elemencie
+    //zdobycie nazwy miasta i zmiana na widgecie
+          var latlng = e.geocode.center;
+          
+      let nameArr = e.geocode.name.split(",");
+      console.log(nameArr);
+      console.log("sama nazwa: " + nameArr[0]);
+      const span = <HTMLElement>document.getElementById('cityName');
+      span.textContent = nameArr[0];
+
+      //sprawdzenie pogody po zmianie chyba
+      const myTimeout = setTimeout(function(){
+      const box = document.getElementById('testpogody') as HTMLDivElement | null;
+      let sprawdzam = (<HTMLElement>document.getElementById("testpogody")).nodeValue;
+      console.log(box?.innerHTML );
+      let url;
+      //wybór zdjecia do pogody
+      //  Snow, Thunderstorm,   
+      if(box?.innerHTML == "Haze" || box?.innerHTML == "Clouds"){
+        url = 'assets/day/cloudy.svg';
+      }
+      else if(box?.innerHTML == "Rain") {
+        url = 'assets/rainy-6.svg';
+      }
+      else if(box?.innerHTML == "Thunderstorm") {
+        url = 'assets/thunder.svg';
+      }
+      else if(box?.innerHTML == "Snow") {
+        url = 'assets/snowy-6.svg';
+      }
+      else {
+        url = 'assets/day/day.svg';
+      }
+      var greenIcon = L.icon({
+            
+        iconUrl: url,
+
+        iconSize:     [150, 250], // size of the icon
+        shadowSize:   [100, 200], // size of the shadow
+        iconAnchor:   [90, 220], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+ });
+
+      var marker = L.marker(latlng,{icon: greenIcon}).addTo(map);
+    }, 3500);
+      map.fitBounds(e.geocode.bbox);
+    })
+    .addTo(map);
 
    //adding custom marker at startpoint
-   const url =  'assets/rainy-1.svg';
-    var customIcon = L.icon({
-      
-      iconUrl: url,
-      iconSize:     [100, 150], // size of the icon
-      shadowSize:   [50, 64], // size of the shadow
-      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62],  // the same for the shadow
-      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-  });
-  L.marker([position.coords.latitude, position.coords.longitude], {icon: customIcon}, {alt: 'ogór rick'}).addTo(map) .bindPopup('You are here :)') ;
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
+   const myTimeout = setTimeout(function(){
+    let url =  'assets/rainy-1.svg';
+    //wybór zdjecia do pogody
+    const box = document.getElementById('testpogody') as HTMLDivElement | null;
+    if(box?.innerHTML == "Haze" || box?.innerHTML == "Clouds"){
+      url = 'assets/day/cloudy.svg';
+    }
+    else if(box?.innerHTML == "Rain") {
+      url = 'assets/rainy-6.svg';
+    }
+    else if(box?.innerHTML == "Thunderstorm") {
+      url = 'assets/thunder.svg';
+    }
+    else if(box?.innerHTML == "Snow") {
+      url = 'assets/snowy-6.svg';
+    }
+    else {
+      url = 'assets/day/day.svg';
+    }
+      var customIcon = L.icon({
+        
+        iconUrl: url,
+        iconSize:     [100, 150], // size of the icon
+        shadowSize:   [50, 64], // size of the shadow
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+    L.marker([position.coords.latitude, position.coords.longitude], {icon: customIcon}, {alt: 'ogór rick'}).addTo(map) .bindPopup('You are here :)') ;
+          L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+   }, 3500);
+   
+
+  
+  
 
    });
 
@@ -56,4 +141,34 @@ export class AppComponent implements OnInit{
    
 
   }
+  getCity(coordinates: any[]) {
+    var xhr = new XMLHttpRequest();
+    var lat = coordinates[0];
+    var lng = coordinates[1];
+  
+    // Paste your LocationIQ token below.
+    xhr.open('GET', "https://us1.locationiq.com/v1/reverse.php?" + window.atob("a2V5PXBrLjNkNWFjNDc4NjQzODgzNTU1ZWIyNDg4MjM4NTQ0ODlhJmxhdD0=") +
+    lat + "&lon=" + lng + "&format=json", true);
+    xhr.send();
+    xhr.onreadystatechange = processRequest;
+    xhr.addEventListener("readystatechange", processRequest, false);
+  
+    function processRequest(e: any) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var response = JSON.parse(xhr.responseText);
+            let city;
+            //checking if its a village
+            if( response.address.town != null) {
+              city = response.address.town;
+            } else {
+              city = response.address.village;
+            }
+            
+            console.log(city);
+            const span = <HTMLElement>document.getElementById('cityName');
+          span.textContent = city;
+            return city;
+        }
+    }
+}
 }
